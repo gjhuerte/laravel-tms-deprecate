e71d9624d8cb09f39d085880645bcfa565de2dc3
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-08-16 19:29:46 +0800

    Added initial ticket comments table

---
+++ database/migrations/2018_08_16_112708_create_ticket_comments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->longText('details');
+            $table->integer('authored_by')->unsigned();
+            $table->foreign('authored_by')
+                    ->references('id')
+                    ->on('users')
+                    ->onUpdate('cascade')
+                    ->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_comments');
+    }
+}
