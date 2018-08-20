a65371cc718856e30a11ab4cdfd019252d45b555
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-08-20 16:44:16 +0800

    Added merge module for merging array separated values

--- app/Http/Packages/Tag/TagManager.php
+++ app/Http/Packages/Tag/TagManager.php
@@ -8,7 +8,7 @@ class TagManager
     /**
      * Sanitize passed string and returns it in array format
      * 
-     * @param string $string comma separated string
+     * @param string $string separated by certain delimiter
      * @param constant $filter filter to be used. default string
      *
      * @return list of all tags
@@ -28,13 +28,28 @@ class TagManager
     /**
      * Pass a string and returns array splitted by delimiter
      * 
-     * @param $string accepts string separated by delimiter
+     * @param string $string accepts string separated by delimiter
+     * @param string $delimiter delimiter for string to be separated. default comma
      * 
      * @return array $string split by using explode
      */
     public static function split($string, $delimiter = ',')
     {
-        $string = explode($delimiter, $string);
+        $arrayList = explode($delimiter, $string);
+        return $arrayList;
+    }
+
+    /**
+     * Pass an array and returns a single string separated by delimiter
+     * 
+     * @param array $array accepts array list of items
+     * @param string $delimiter delimiter for string to be separated. default comma
+     * 
+     * @return string $string split by using explode
+     */
+    public static function merge($array, $delimiter = ',')
+    {
+        $string = implode($delimiter, $array);
         return $string;
     }
 }
