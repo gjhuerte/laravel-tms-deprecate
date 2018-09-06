218d705093e6094f2bcb6ce902ba7537f084c2a9
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-06 10:37:49 +0800

    Adjust the header for dashboard

--- resources/views/dashboard/base-layout.blade.php
+++ resources/views/dashboard/base-layout.blade.php
@@ -3,24 +3,24 @@
 @section('content')
 <div class="container-fluid mt-3 p-3">
 	<div class="row">
-		<div class="col-md-3 my-3">
+		<div class="col-md-3 my-1">
+			<p class="text-center h3 mb-3">Messages</p>
 			<div class="card">
 				<div class="card-body">
-					<p class="text-center display-4">Messages</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-6 my-3">
+		<div class="col-md-6 my-1">
+			<p class="text-center h3 mb-3">Announcements</p>
 			<div class="card">
 				<div class="card-body">
-					<p class="text-center display-4">Announcements</p>
 				</div>
 			</div>
 		</div>
-		<div class="col-md-3 my-3">
+		<div class="col-md-3 my-1">
+			<p class="text-center h3 mb-3">Reports</p>
 			<div class="card">
 				<div class="card-body">
-					<p class="text-center display-4">Reports</p>
 				</div>
 			</div>
 		</div>
