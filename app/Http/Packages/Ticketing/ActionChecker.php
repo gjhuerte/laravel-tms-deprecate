683e078e75e88209b5d7c772976a9df904f9cda9
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-10 13:28:38 +0800

    Add documentation comment for isClosed function

--- app/Http/Packages/Ticketing/ActionChecker.php
+++ app/Http/Packages/Ticketing/ActionChecker.php
@@ -6,6 +6,13 @@ use App\Http\Interfaces\Ticket\ActionChecker as ActionCheckerInterface;
 
 class ActionChecker implements ActionCheckerInterface
 {
+
+	/**
+	 * Check if the status of the ticket is closed or not
+	 * 
+	 * @param  int     $ticketId ticket id for checking
+	 * @return boolean           true if the ticket action is closed else false
+	 */
 	protected function isClosed(int $ticketId)
 	{
 		return true;
