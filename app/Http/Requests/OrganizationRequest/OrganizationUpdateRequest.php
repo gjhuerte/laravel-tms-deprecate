aafe616abc8c1aa2a49d4d8f9012b3258faa832c
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-12-15 19:57:22 +0800

    Organization validations

--- app/Http/Requests/OrganizationRequest/OrganizationUpdateRequest.php
+++ app/Http/Requests/OrganizationRequest/OrganizationUpdateRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\MaintenanceRequest;
+namespace App\Http\Requests\OrganizationRequest;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class MaintenanceStoreRequest extends FormRequest
+class OrganizationUpdateRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -13,7 +13,7 @@ class MaintenanceStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return false;
     }
 
     /**
