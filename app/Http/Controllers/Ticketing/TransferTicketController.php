cb1555d9a2f2b3be4cb509c137f56e03b91ad3e7
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-12-21 17:56:30 +0800

    Create functionality for transferring ticket

--- app/Http/Controllers/Ticketing/TransferTicketController.php
+++ app/Http/Controllers/Ticketing/TransferTicketController.php
@@ -7,5 +7,25 @@ use App\Http\Controllers\Controller;
 
 class TransferTicketController extends Controller
 {
-    //
+
+    /**
+     * Display the form for transferring the
+     * ticket to other personnel
+     *
+     * @return void
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Submits the list to the server
+     *
+     * @return void
+     */
+    public function store()
+    {
+
+    }
 }
